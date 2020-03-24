47a56ddf979515969f547d665ed104ab450a2fff
sharathsamala <sharathsamala@live.com>
2020-03-24 18:49:04 +0530

    Added user auth and registration

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from db import db, mongo_db
+from security import authenticate, identity
+from resources.user import UserRegister
+
+
+app = Flask(__name__)
+
+# User Auth DB configs
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'jose'
+
+# Mongo db configs
+app.config['MONGOALCHEMY_DATABASE'] = 'UserProfiles'
+api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity)  # /auth
+api.add_resource(UserRegister, "/register")
+
+
+if __name__ == '__main__':
+    try:
+        os.remove("/Users/sharathsamala/Desktop/all/Projects/pythonProjects/UserProfiles/data.db")
+    except:
+        pass
+
+    db.init_app(app)
+    mongo_db.init_app(app)
+    app.run(port=5000, debug=True)
---
+++ db.py
@@ -0,0 +1,11 @@
+from flask_mongoengine import MongoEngine
+from flask_sqlalchemy import SQLAlchemy
+
+mongo_db = MongoEngine()
+db = SQLAlchemy()
+
+
+
+
+
+## "mongodb+srv://admin:admin123@cloud1-dhhxk.mongodb.net/test?retryWrites=true&w=majority"
